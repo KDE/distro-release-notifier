4bfd2191a14c7332c5dc0f151d37bccb4f99a1d7
Harald Sitter <sitter@kde.org>
2020-11-11 13:28:37 +0100

    make fully reuse compliant
    + all cmake is BSD-3-Clause
    + all code is GPL-2.0-only OR GPL-3.0-only OR
    LicenseRef-KDE-Accepted-GPL
    + all not so interesting extra data is CC0-1.0

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2018 Jonathan Riddell <jr@jriddell.org>
+
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/distro-release-notifier.pot
 rm -f rc.cpp
--- src/config.h.cmake
+++ src/config.h.cmake
@@ -1,1 +1,6 @@
+/*
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+    SPDX-FileCopyrightText: 2018 Harald Sitter <sitter@kde.org>
+*/
+
 #cmakedefine01 NAME_FROM_FLAVOR
